70d4a3ddcfebaa7943e76156f14212f12b64124c
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-19 16:57:44 +0530

    Final Caeser Cipher

---
+++ CaeserCipherTest/CaeserCipherTest.cs
@@ -0,0 +1,49 @@
+using CaeserCipherAlgorithm;
+
+namespace EncryptDecryptTest
+{
+    [TestClass]
+    public class CaeserCipherTest
+    {
+        [TestMethod]
+        public void EncryptTestTextFactor3()
+        {
+            //--Arrange
+
+            CaeserCipher obj = new CaeserCipher();
+            string expected = "dev123Xwe";
+            //--Act
+            string actual = obj.DoCeaserCipher("abs123Utb", 3);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EncryptTest()
+        {
+            //--Arrange
+
+            CaeserCipher obj = new CaeserCipher();
+            string expected = "igDcHa N90 32";
+            //--Act
+            string actual = obj.DoCeaserCipher("zxUtYr E90 32", 9);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void DecryptTestMethod1()
+        {
+            //--Arrange
+
+            CaeserCipher obj = new CaeserCipher();
+            string expected = "qoLkPi V90 32";
+            //--Act
+            string actual = obj.DoCeaserCipher("zxUtYr E90 32", -9);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
